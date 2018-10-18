a36b79a83023f7408cd627af9e5a6958887e889a
romulo <romulo@unimestre.com>
2018-10-18 18:02:21 -0300

    apis com node

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const objConexao = mysql.createConnection({
    host: 'localhost',
    user: 'root',
    password: '',
    database: 'ci_teste'
 });
 
-connection.connect(function (err) {
+objConexao.connect(function (err) {
    if (err) {
       console.error('error connecting: ' + err.stack);
       return;
    }
-   console.log('connected as id ' + connection.threadId);
+   console.log('connected as id ' + objConexao.threadId);
 }) ;
 
 const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-   connection.query('SELECT * FROM pessoas', function (error, results, fields) {
+   objConexao.query('SELECT * FROM pessoas', function (error, results, fields) {
       if (error) throw error;
       res.send(results);
       // console.log('The solution is: ', results[0].solution);
    });
 });
+
+app.post('/inserir',(req, res) => {
+
+   // var objPessoaInserir  = {
+   //    nm_pessoa: 'Pessoa Cadastrada VIA API!',
+   //    fone: '(47)991725457',
+   //    email: 'emailVindoDaApi@gmail.com',
+   // };
+
+   console.log(req);
+
+   // var query = objConexao.query(
+   //    'INSERT INTO pessoas SET ?',
+   //    objPessoaInserir,
+   //    function (error, results, fields) {
+   //       if (error) throw error;
+   //       res.send(results);
+   //       console.log(query.sql);
+   //    }
+   // );
+
+
+});
+
 
 app.listen(3001);
